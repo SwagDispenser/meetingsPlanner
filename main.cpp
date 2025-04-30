fa7cf22c490d4a4e7a07c609cfa7733eb2f833ca
SwagDispenser <dimadovgesh@gmail.com>
2025-04-30 09:01:47 +0300

    Added menu class realisation

--- main.cpp
+++ main.cpp
@@ -1,50 +1,33 @@
 #include <iostream>
 #include <vector>
-#include "Organizer.h"
-#include "RegularParticipant.h"
+#include <memory>
+#include "Menu.h"
 #include "Meeting.h"
-#include "InfoHandler.h"
-#include "Participant.h"
-#include "Location.h"
-#include "VipParticipant.h"
 
 int main() {
-    std::cout << "\nOrganizer:\n";
-    Organizer org1("Іван", "ivan@example.com", "HR");
-    Organizer org2(org1);
-    Organizer org3(std::move(org1));
-    std::cout << "Org2: " << org2.getName() << ", " << org2.getEmail() << ", " << org2.getDepartment() << std::endl;
-    std::cout << "Org3: " << org3.getName() << ", " << org3.getEmail() << ", " << org3.getDepartment() << std::endl;
+    std::vector<std::shared_ptr<Meeting>> meetings;
+    Menu::loadMeetings(meetings);
 
-    std::cout << "\nRegularParticipant:\n";
-    RegularParticipant p1("Марія", "maria@example.com", "Менеджер", "TechCorp", true);
-    RegularParticipant p2(p1);
-    RegularParticipant p3(std::move(p1));
-    std::cout << "P2: " << p2.getName() << ", " << p2.getEmail() << ", " << p2.getCompany() << std::endl;
-    std::cout << "P3: " << p3.getName() << ", " << p3.getEmail() << ", " << p3.getCompany() << std::endl;
+    int role;
+    std::cout << "Вітаємо в програмі-планувальнику зустрічей!\n";
+    std::cout << "1. Увійти як користувач\n2. Увійти як адміністратор\nОберіть роль: ";
+    std::cin >> role;
 
-    std::cout << "\nMeeting:\n";
-    Location loc("Офіс", "вул. Головна, 12", "Україна");
-    Meeting meet1("Річна зустріч", "Обговорення результатів", "2025-04-10 10:00", org2, loc);
-    meet1.addParticipant(p2);
-    meet1.addParticipant(p3);
+    if (role == 1) {
+        Menu::userMenu(meetings);
+    } else if (role == 2) {
+        std::string password;
+        std::cout << "Введіть пароль адміністратора: ";
+        std::cin >> password;
 
-    Meeting meet2(meet1);
-    Meeting meet3(std::move(meet1));
+        if (password == Menu::ADMIN_PASSWORD) {
+            Menu::adminMenu(meetings);
+        } else {
+            std::cout << "Невірний пароль!\n";
+        }
+    } else {
+        std::cout << "Невідома опція!\n";
+    }
 
-    std::cout << "Meet2: " << meet2.getTitle() << ", Організатор: " << meet2.getOrganizer().getName() << std::endl;
-    std::cout << "Meet3: " << meet3.getTitle() << ", Організатор: " << meet3.getOrganizer().getName() << std::endl;
-
-    std::vector<InfoHandler*> infos;
-    infos.push_back(&org2);
-    infos.push_back(&p2);
-    infos.push_back(&meet2);
-
-    RegularParticipant base;
-    VipParticipant derived;
-    base.doStaticThing();
-    derived.doStaticThing();
-    RegularParticipant* pb = new VipParticipant;
-    pb->doStaticThing();
     return 0;
 }
